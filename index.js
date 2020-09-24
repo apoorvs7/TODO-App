f246cb350f4528e9840e45132e40bdd8f8af82e6
apoorvs7 <apoorvsingh52@gmail.com>
2020-09-24 13:33:09 +0530

    Set the server and it is running.

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const port = 8000;
+
+const app = express();
+
+
+
+
+
+app.listen(port, function(err){
+    if(err){
+        console.log(`Error in running the server: ${err}`);
+        return;
+    }
+
+    console.log(`Server up and running on port: ${port}`);
+});
